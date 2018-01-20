5019bd9f45d18f17e8b9c7d331c838044f2e4878
Aishwarya Chandrashekhar <achandrashekhar@usfca.edu>
2018-01-19 17:02:14 -0800

    enzyme test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow,mount } from 'enzyme'
 import App from './App';
+import SearchResult from './SearchResult';
+import NavigateHome from './NavigateHome';
+
+
+const app = shallow(<App/>)
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('initilizes with the right state', () =>{
+  expect(app.state().screenName).toEqual("search")
+});
+
+it('changes state properly',()=>{
+  const navigatehome = shallow(<NavigateHome/>)
+  navigatehome.find('.back').first().simulate('click')
+  expect(app.state().screenName).toEqual("search")
 });
